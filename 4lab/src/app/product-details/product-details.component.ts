dc52fc7b9dd27062ef4f35feffc2d0ea4a59bc19
moldakasyma <moldakasyma@mail.ru>
2024-02-21 18:18:19 +0600

    updated

--- 4lab/src/app/product-details/product-details.component.ts
+++ 4lab/src/app/product-details/product-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
-
+import { CartService } from '../cart.service';
 
 
 @Component({
@@ -15,8 +15,12 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product | undefined;
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
   
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit() {
     // First get the product id from the current route.
@@ -28,3 +32,5 @@ export class ProductDetailsComponent implements OnInit {
   }
   
 }
+
+
--- 4lab/src/app/product-list/product-list.component.ts
+++ 4lab/src/app/product-list/product-list.component.ts
@@ -12,7 +12,7 @@ export class ProductListComponent {
   products = [...products];
 
   share(url: string) {
-    var sharelink = "https://t.me/share/url?url="+url+"&text=Hello! I'd like to but it!";
+    var sharelink = "https://api.whatsapp.com/send?text=".concat(url)+"&text=Hello! I'd like to buy it! Can you give more info about the product?";
     window.open(sharelink);
   }
 
